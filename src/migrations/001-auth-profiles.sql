efe92656c03047f2d366f24c608e95d31ab07fb6
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-08-06 14:58:59 +0700

    feat: adding auth-profiles.sql migration file

---
+++ src/migrations/001-auth-profiles.sql
@@ -0,0 +1,50 @@
+---create table profiles
+create table public.profiles (
+  id uuid not null references auth.users on delete cascade,
+  name text,
+  role text,
+  avatar_url text,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+
+  ---untuk set primary key
+  primary key (id)
+);
+
+---untuk table security
+alter table public.profiles enable row level security;
+
+---insert row into public.profiles when user is created
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.profiles (id, name, role, avatar_url)
+  values (new.id, new.raw_user_meta_data ->> 'name', new.raw_user_meta_data ->> 'role', new.raw_user_meta_data ->> 'avatar_url');
+  return new;
+end;
+$$;
+
+---trigger the function public.handle_new_user every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+---delete a row from public.profiles
+create function handle_delete_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  delete from public.profiles where id = old.id;
+  return old;
+end;
+$$;
+
+---trigger the function public.handle_new_user every time a user is deleted
+create trigger on_auth_user_deleted
+  after delete on auth.users
+  for each row execute procedure public.handle_delete_user();
