376a8088d23c96a15de4683d9e4fac9dc8261bb6
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-08-18 10:13:33 +0700

    feat: initial project

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -1,52 +1,89 @@
-"use client"
+"use client";
 
-import { Card, CardDescription, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-import { loginSchema, LoginForm } from "@/validations/auth-validation";
-import { INITIAL_LOGIN_FORM } from "@/constants/auth-constant";
+import { loginSchemaForm, LoginForm } from "@/validations/auth-validation";
+import {
+  INITIAL_LOGIN_FORM,
+  INITIAL_STATE_LOGIN_FORM,
+} from "@/constants/auth-constant";
 import FormInput from "@/components/common/form-input";
+import { startTransition, useActionState } from "react";
+import { login } from "../actions";
 
 export default function Login() {
+  //instance form
+  const form = useForm<LoginForm>({
+    resolver: zodResolver(loginSchemaForm), //panggil type dan skema form dari auth-validation.ts
+    defaultValues: INITIAL_LOGIN_FORM,
+  });
 
-    //instance form
-    const form = useForm<LoginForm>({
-        resolver: zodResolver(loginSchema), //panggil type dan skema form dari auth-validation.ts
-        defaultValues: INITIAL_LOGIN_FORM,
-    })
+  const [loginState, loginAction, isPendingLogin] = useActionState(
+    login,
+    INITIAL_STATE_LOGIN_FORM,
+  );
 
-    function onSubmit(data: z.infer<typeof loginSchema>) {
-        console.log(data);
-    }
+  //fungsi ketika tombol submit ditekan dan form berfungsi
+  const onSubmit = form.handleSubmit(async (data) => {
+    const formData = new FormData();
+    Object.entries(data).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    startTransition(() => {
+      loginAction(formData);
+    });
+  });
 
-    return (
-        <Card>
-            <CardHeader className="text-center">
-                <CardTitle className="text-xl">
-                    Welcome
-                </CardTitle>
-                <CardDescription>Login to access all features</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form id="login-form" onSubmit={form.handleSubmit(onSubmit)}>
-                    <div className="flex flex-col gap-4">
-                        <FormInput form={form} name="email" label="Email" type="email" placeholder="Insert Your Email" />
-                        <FormInput form={form} name="password" label="Password" type="password" placeholder="Insert Your Password" />
-                    </div>
-                </form>
-            </CardContent>
-            <CardFooter>
-                <div className="flex flex-row gap-3">
-                    <Button type="button" variant={'outline'} onClick={() => form.reset()}>
-                        Reset
-                    </Button>
-                    <Button type="submit" form="login-form">
-                        Submit
-                    </Button>
-                </div>
-            </CardFooter>
-        </Card>
-    )
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Welcome</CardTitle>
+        <CardDescription>Login to access all features</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="login-form" onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-4">
+            <FormInput
+              form={form}
+              name="email"
+              label="Email"
+              type="email"
+              placeholder="Insert Your Email"
+            />
+            <FormInput
+              form={form}
+              name="password"
+              label="Password"
+              type="password"
+              placeholder="Insert Your Password"
+            />
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <div className="flex flex-row gap-3">
+          <Button
+            type="button"
+            variant={"outline"}
+            onClick={() => form.reset()}
+          >
+            Reset
+          </Button>
+          <Button type="submit" form="login-form">
+            Submit
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  );
 }
---
+++ src/app/(auth)/login/actions.ts
@@ -0,0 +1,63 @@
+'use server'
+
+import { createClient } from "@/lib/supabase/server";
+import { AuthFormState } from "@/types/auth";
+import { loginSchemaForm } from "@/validations/auth-validation";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+export async function login(prevState: AuthFormState, formData: FormData) {
+    //validasi data inputan dari user
+    const validatedFields = loginSchemaForm.safeParse({
+        email: formData.get('email'),
+        password: formData.get('password'),
+    })
+
+    //jikalau gagal validasi
+    if (!validatedFields.success) {
+        return {
+            status: 'error',
+            errors: validatedFields.error.flatten().fieldErrors,
+        };
+    }
+
+    //jikalau lolos validasi
+    const supabase = await createClient();
+
+    //log in menggunakan data dari inputan yang sudah di validasi
+    const { error, data } = await supabase.auth.signInWithPassword(validatedFields.data);
+
+    //jikalau gagal/error mengambil data
+    if (error) {
+        return {
+            status: "error",
+            errors: {
+                ...prevState.errors,
+                _form: [error.message]
+            }
+        }
+    }
+
+    //ambil data user
+    const { data: profile } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('id', data.user?.id)
+        .single();
+
+    //letakkan data user tersebut ke dalam cookies
+    if(profile) {
+        const cookiesStore = await cookies();
+        cookiesStore.set('user_profile', JSON.stringify(profile), {
+            httpOnly: true,
+            path: '/',
+            sameSite: 'lax',
+            maxAge: 60 * 60 * 24 * 365,
+        });
+    }
+
+    revalidatePath('/', 'layout'); //buang cache sebelumnya
+    redirect('/'); //next/navigation
+
+}
