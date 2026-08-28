6efc3a986f1769cd646ca56d04399b9f85bcd54f
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-08-28 10:39:19 +0700

    feat: make menu data schema

---
+++ src/migrations/002-menu.sql
@@ -0,0 +1,15 @@
+create table public.menus (
+  id serial not null,
+  name text,
+  description text,
+  price numeric,
+  image_url text,
+  category text,
+  is_available boolean,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+
+  primary key (id)
+);
+
+alter table public.menus enable row level security;
