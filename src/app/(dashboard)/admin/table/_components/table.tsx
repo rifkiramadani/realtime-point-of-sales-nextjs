60df565da4a583cca28d84910476822251bad6e0
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-08-29 10:04:32 +0700

    feat: displaying list table data

---
+++ src/app/(dashboard)/admin/table/_components/table.tsx
@@ -0,0 +1,183 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { createClient } from "@/lib/supabase/client";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+import DataTable from "@/components/common/data-table";
+import { useCallback, useMemo, useState } from "react";
+import DropdownAction from "@/components/common/dropdown-action";
+import { Pencil, Trash2 } from "lucide-react";
+import useDataTable from "@/hooks/use-data-table";
+import { cn } from "@/lib/utils";
+import { Table } from "@/validations/table-validation";
+import { HEADER_TABLE_TABLE } from "@/constants/table-constant";
+
+const TableManagement = () => {
+  const supabase = createClient();
+  const {
+    currentPage,
+    currentLimit,
+    currentSearch,
+    handleChangeLimit,
+    handleChangePage,
+    handleChangeSearch,
+  } = useDataTable();
+  const {
+    data: tables,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["tables", currentPage, currentLimit, currentSearch],
+    queryFn: async () => {
+      const query = supabase
+        .from("tables")
+        .select("*", { count: "exact" })
+        .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
+        .order("created_at");
+
+      if (currentSearch) {
+        query.or(
+          `name.ilike.%${currentSearch}%,capacity.ilike.%${currentSearch}%,status.ilike.%${currentSearch}%`,
+        );
+      }
+
+      const result = await query;
+
+      if (result.error) {
+        toast.error("Get Table data failed", {
+          description: result.error.message,
+        });
+        throw new Error(result.error.message);
+      }
+      return result ?? [];
+    },
+  });
+
+  const [open, setOpen] = useState(false);
+
+  const [selectedAction, setSelectedAction] = useState<{
+    data: Table;
+    type: "update" | "delete";
+  } | null>();
+
+  const handleCreateSuccess = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const handleChangeAction = useCallback((open: boolean) => {
+    if (!open) {
+      setSelectedAction(null);
+    }
+  }, []);
+
+  const filteredData = useMemo(() => {
+    return (tables?.data || []).map((table: Table, index) => {
+      return [
+        currentLimit * (currentPage - 1) + index + 1,
+        <div>
+          <h4 className="font-bold">{table.name}</h4>
+          <p className="text-xs">{table.description}</p>
+        </div>,
+        table.capacity,
+        <div
+          className={cn("px-2 py-1 rounded-full text-white w-fit capitalize", {
+            "bg-green-600": table.status === "available",
+            "bg-red-600": table.status === "unavailable",
+            "bg-yellow-600": table.status === "reserved",
+          })}
+        >
+          {table.status}
+        </div>,
+        <DropdownAction
+          menu={[
+            {
+              label: (
+                <span className="flex items-center gap-2">
+                  <Pencil />
+                  Edit
+                </span>
+              ),
+              action: () => {
+                setSelectedAction({
+                  data: table,
+                  type: "update",
+                });
+              },
+            },
+            {
+              label: (
+                <span className="flex items-center gap-2">
+                  <Trash2 className="text-red-400" />
+                  Delete
+                </span>
+              ),
+              variant: "destructive",
+              action: () => {
+                setSelectedAction({
+                  data: table,
+                  type: "delete",
+                });
+              },
+            },
+          ]}
+        />,
+      ];
+    });
+  }, [tables, currentLimit, currentPage]);
+
+  const totalPages = useMemo(() => {
+    return tables && tables.count != null
+      ? Math.ceil(tables.count / currentLimit)
+      : 0;
+  }, [tables, currentLimit]);
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
+        <h1 className="text-2xl font-bold">Table Management</h1>
+        <div className="flex gap-2">
+          <Input
+            placeholder="Search by name, capacity and status"
+            onChange={(event) => handleChangeSearch(event.target.value)}
+          />
+          {/* <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger
+              render={<Button variant={"outline"}>Create</Button>}
+            />
+            <DialogCreateMenu
+              refetch={refetch}
+              onSuccess={handleCreateSuccess}
+            />
+          </Dialog> */}
+        </div>
+      </div>
+      <DataTable
+        header={HEADER_TABLE_TABLE}
+        data={filteredData}
+        isLoading={isLoading}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        currentLimit={currentLimit}
+        onChangePage={handleChangePage}
+        onChangeLimit={handleChangeLimit}
+      />
+      {/* <DialogUpdateMenu
+        open={selectedAction !== null && selectedAction?.type === "update"}
+        refetch={refetch}
+        currentData={selectedAction?.data}
+        handleChangeAction={handleChangeAction}
+      />
+      <DialogDeleteMenu
+        open={selectedAction !== null && selectedAction?.type === "delete"}
+        refetch={refetch}
+        currentData={selectedAction?.data}
+        handleChangeAction={handleChangeAction}
+      /> */}
+    </div>
+  );
+};
+
+export default TableManagement;
---
+++ src/app/(dashboard)/admin/table/page.tsx
@@ -0,0 +1,11 @@
+import TableManagement from "./_components/table";
+
+export const metadata = {
+  title: "Ignaciasz Caffe | Table Management",
+};
+
+const MenuManagementPage = () => {
+  return <TableManagement />;
+};
+
+export default MenuManagementPage;
