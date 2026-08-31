917145dcbd8128b8763d069319019c6bf7ef0f6d
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-08-31 17:40:14 +0700

    feat: implementing handle success payment

---
+++ src/app/payment/failed/page.tsx
@@ -0,0 +1,11 @@
+import Failed from "./_components/failed";
+
+export const metadata = {
+  title: "Ignaciasz Caffe | Payment Failed",
+};
+
+const FailedPage = () => {
+  return <Failed />;
+};
+
+export default FailedPage;
---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,11 @@
+import Success from "./_components/success";
+
+export const metadata = {
+  title: "Ignaciasz Caffe | Payment Success",
+};
+
+const SuccessPage = () => {
+  return <Success />;
+};
+
+export default SuccessPage;
