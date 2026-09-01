9ac50ec7916cf6eca01619f82b0725275c33ae53
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-09-01 13:22:12 +0700

    feat: updating next.js package to secure version

--- src/lib/supabase/default.ts
+++ src/lib/supabase/default.ts
@@ -1,9 +1,20 @@
 import { environment } from "@/configs/environment";
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+
+// Buat variabel global untuk menampung instance tunggal
+let supabaseInstance: SupabaseClient | null = null;
 
 export function createClientSupabase() {
-  return createClient(
+  // Jika instance sudah pernah dibuat, gunakan kembali (tidak bikin baru)
+  if (supabaseInstance) {
+    return supabaseInstance;
+  }
+
+  // Jika belum ada, buat baru sekali saja
+  supabaseInstance = createClient(
     environment.SUPABASE_URL!,
     environment.SUPABASE_PUBLISHABLE_KEY!,
   );
+
+  return supabaseInstance;
 }
